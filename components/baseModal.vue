2409fd659439f3353704bd406727bf556d232070
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2023-06-27 15:45:44 -0300

    commit design
    commit design

---
+++ components/baseModal.vue
@@ -0,0 +1,164 @@
+<template>
+    <div id="modalProtect" class="modal">
+        <section class="modal-content">
+
+            <div class="modal-close">
+                <button type="button" name="close" class="btn-close">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M13.9844 1.42188L8.40625 7L13.9844 12.5781L12.5781 13.9844L7 8.40625L1.42188 13.9844L0.015625 12.5781L5.59375 7L0.015625 1.42188L1.42188 0.015625L7 5.59375L12.5781 0.015625L13.9844 1.42188Z"
+                            fill="#6E6E6E" />
+                    </svg>
+                </button>
+            </div>
+
+            <div class="content-padding">
+
+                <div class="modal-header mb-4">
+                    <slot name="header"></slot>
+
+                </div>
+
+                <div class="body">
+                    <form action="post" class="mb-2">
+                        <slot name="body"></slot>
+
+                        <div class="btnBody">
+                            <button type="submit" class="btnModal btn-primary text-uppercase"> {{ props.nameBtn }}</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="modal-footer mb-4">
+                    <slot name="footer"></slot>
+
+                </div>
+
+                <slot name="mini_footer"></slot>
+
+            </div>
+        </section>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    nameBtn: { type: String, required: true },
+}) 
+</script>
+
+<style lang="scss" scoped>
+.modal {
+    display: block;
+    position: fixed;
+    z-index: 2;
+    padding-top: 80px;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4);
+
+
+    .modal-content {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        position: relative;
+        margin: auto;
+        padding: 0;
+        // width: 50%;
+        width: 100%;
+        max-width: 694px;
+        height: 746px;
+
+        border-radius: 8px;
+        background-color: $modelsMobile;
+
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.10);
+        -webkit-animation-name: animatetop;
+        -webkit-animation-duration: 1s;
+        animation-name: animatetop;
+        animation-duration: 1s;
+
+        .modal-close {
+            display: flex;
+            width: 100%;
+            height: 52.56px;
+            padding: 24px 64px 14.56px 12px;
+            flex-direction: column;
+            justify-content: center;
+            align-items: flex-end;
+
+            button {
+                background: transparent;
+                color: #6E6E6E;
+                text-align: center;
+                font-size: 24px;
+
+                cursor: pointer;
+            }
+        }
+
+        .content-padding {
+            padding: 0 64px 32px 64px;
+
+        }
+
+        .modal-header {
+            display: flex;
+        }
+
+        .body {
+            display: flex;
+            flex-direction: column;
+            gap: 24px;
+        }
+
+        .btnBody {
+            margin-top: 24px;
+        }
+
+        .btnModal {
+            width: 100%;
+            font-family: Inter-Medium;
+            font-weight: 500;
+            line-height: 27.134px;
+        }
+
+        .modal-footer {
+            display: flex;
+            flex-direction: column;
+            gap: 24px;
+        }
+
+    }
+
+    @-webkit-keyframes animatetop {
+        from {
+            top: 0;
+            opacity: 0
+        }
+
+        to {
+            top: 0;
+            opacity: 1
+        }
+    }
+
+    @keyframes animatetop {
+        from {
+            top: -5px;
+            opacity: 0
+        }
+
+        to {
+            top: 0;
+            opacity: 1
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,77 @@
 <template>
     <main>
+        <LazyBaseModal :nameBtn="'Criar Conta'">
+            <template v-slot:header>
+                <div class="welcome">
+                    <h1 class="fs-4 text-uppercase">Bem-Vindo</h1>
+                    <span>Registre-se para criar uma conta e proteger seus acessos</span>
+                </div>
+            </template>
+            <template v-slot:body>
+                <div class="card-body">
+                    <div class="form-input">
+                        <input type="text" placeholder="Email*">
+                    </div>
+
+                    <div class="form-input">
+                        <input type="text" placeholder="CPF*">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" placeholder="Senha*">
+                        <span>Deve conter no mínimo 6 dígitos</span>
+
+                        <div class="forgotPassword">
+                            <a href=""><span class="text-uppercase">Esqueceu a senha?</span></a>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="form-check">
+                    <div class="check">
+                        <input type="checkbox" name="validForm" id="validForm" />
+                    </div>
+
+                    <span>Li e aceito os Termos de Uso e Política de Privacidade</span>
+                </div>
+
+            </template>
+
+            <template v-slot:footer="">
+                <div class="diviser">
+                    <hr />
+                    <span>Ou acesse com</span>
+                    <hr />
+                </div>
+
+                <div class="socialsBtn">
+                    <button type="button" class="btnSocials">
+                        <img src="../assets/img/apple.svg" alt="apple" width="24px" height="24px" />
+
+                        Apple
+                    </button>
+                    <button type="button">
+                        <img src="../assets/img/google.svg" alt="google" width="24px" height="24px" />
+                        Google
+                    </button>
+                </div>
+
+            </template>
+
+            <template v-slot:mini_footer>
+                <div class="already">
+                    <span>Já tem conta? Faça <a href="#">login</a> </span>
+                </div>
+            </template>
+        </LazyBaseModal>
         <LazyContentServices></LazyContentServices>
         <LazyContentPartners></LazyContentPartners>
         <LazyContentContract></LazyContentContract>
         <LazyContentFAQ></LazyContentFAQ>
         <LazyContentContactUs></LazyContentContactUs>
+
+
     </main>
 </template>
 
@@ -13,5 +80,186 @@
 <style lang="scss">
 main {
     display: block;
+}
+
+.welcome {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    gap: 8px;
+
+    h1 {
+        text-align: center;
+        font-weight: 600;
+        line-height: 26.4px;
+    }
+
+    span {
+        text-align: center;
+    }
+
+    h1,
+    span {
+        color: white;
+    }
+
+}
+
+.card-body {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    height: auto;
+
+    .form-input,
+    .form-group {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        height: auto;
+        gap: 8px;
+
+        input {
+            height: 48px;
+
+            padding: 10px 12px 10px 12px;
+            border-radius: 4px;
+            border: 0.75px solid var(--grey-grey-1, #333);
+            background: var(--grey-grey-1, #333);
+        }
+
+        span {
+            padding: 0 0.9rem;
+            color: $colorInputText;
+            font-size: 12px;
+            font-family: Inter;
+            font-weight: 500;
+        }
+    }
+
+
+    .forgotPassword {
+        display: flex;
+        justify-content: flex-end;
+        padding: 8px 0px;
+
+        a {
+            text-decoration: none;
+
+            span {
+                color: white;
+                text-align: right;
+                font-size: 12px;
+                line-height: 27.134px;
+                padding: 0;
+                cursor: pointer;
+            }
+
+            &:hover {
+                text-decoration: underline;
+                color: white;
+                text-underline-offset: 3px;
+
+            }
+        }
+    }
+}
+
+.form-check {
+    display: flex;
+    align-items: center;
+    align-self: stretch;
+
+    .check {
+        display: flex;
+        width: 44px;
+        height: 44px;
+        padding: 14px 14px 13px 13px;
+        justify-content: center;
+        align-items: center;
+
+        input {
+            border-radius: 2px;
+            width: 17px;
+            height: 17px;
+
+            &:checked {
+                background: transparent;
+            }
+        }
+
+
+    }
+
+    span {
+        color: white;
+        text-align: center;
+        font-size: 14px;
+        letter-spacing: 1.12px;
+    }
+}
+
+.diviser {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    gap: 30.168px;
+
+    hr {
+        width: 100%;
+        height: 0.943px;
+        border-bottom: 1px solid $colorInputText;
+
+    }
+
+    span {
+        width: 60%;
+        color: $colorInputText;
+        text-align: center;
+        font-size: 14px;
+        font-family: Inter;
+        letter-spacing: 0.905px;
+    }
+
+}
+
+.socialsBtn {
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+    gap: 16px;
+
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 12px;
+        padding: 10px 16px;
+        width: 100%;
+        border-radius: 4px;
+        border: 1px solid #D9D9D9;
+        background-color: #FFF;
+        /* Button */
+        box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.20);
+
+        &:hover {
+            cursor: pointer;
+            background-color: $btnPrimary;
+            box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.1);
+        }
+
+    }
+}
+
+.already {
+    text-align: center;
+    color: white;
+
+    a {
+        color: white;
+    }
 }
 </style>
